f2820b49c66e73995a12b83493a09221eedbe157
Gabriel Palacios <palaju01@luther.edu>
2020-11-10 20:54:38 -0800

    final project all tests passed

--- src/projects/routing/udp_router.py
+++ src/projects/routing/udp_router.py
@@ -25,8 +25,27 @@ def read_config_file(filename: str) -> Tuple[Set, Dict]:
     :param filename: name of the configuration file
     :return tuple of the (neighbors, routing table)
     """
-    raise NotImplementedError
-
+    try:
+        with open(filename, "r") as conf_file:
+            # read the entire file
+            config_str = conf_file.read()
+            # Split file into separate routers
+            routers = config_str.split("\n\n")
+            # create the set and dictionary
+            neighbors = set()
+            routing_table = {}
+            for router in routers:
+                router_list = list(router.split("\n"))
+                if router_list[0] == THIS_HOST: 
+                    for neighbor in router_list[1:]:
+                        # check if there is any neighbor
+                        if neighbor != "":
+                            next_hop, cost = neighbor.split(" ")
+                            neighbors.add(next_hop)
+                            routing_table[next_hop] = [int(cost),next_hop]
+            return neighbors,routing_table
+    except IOError:
+        raise FileNotFoundError("Could not find the specified configuration file {}".format(filename))
 
 def format_update(routing_table: dict) -> bytes:
     """
@@ -35,7 +54,17 @@ def format_update(routing_table: dict) -> bytes:
     :param routing_table: routing table of this router
     :returns the formatted message
     """
-    raise NotImplementedError
+    message = bytearray()
+    message.append(0x0)
+
+    for dest in routing_table:
+        for num in dest.split("."):
+            message.append(int(num))
+
+        message.append(routing_table[dest][0])
+
+    #print("Update Message:", msg)
+    return message
 
 
 def parse_update(msg: bytes, neigh_addr: str, routing_table: dict) -> bool:
@@ -47,7 +76,17 @@ def parse_update(msg: bytes, neigh_addr: str, routing_table: dict) -> bool:
     :param routing_table: this router's routing table
     :returns True is the table has been updated, False otherwise
     """
-    raise NotImplementedError
+    updated = False
+    index = 1   # index
+    while index < len(msg) - 1:
+        addr = str(msg[index]) + "." + str(msg[index+1]) + "." + str(msg[index+2]) + "." + str(msg[index+3])
+        cost = msg[index+4]
+        if addr in routing_table:
+            if routing_table[addr][0] > routing_table[neigh_addr][0]+cost:
+                routing_table[addr][0] = routing_table[neigh_addr][0]+cost
+                updated = True
+        index += 5
+    return updated
 
 
 def send_update(node: str) -> None:
@@ -67,7 +106,19 @@ def format_hello(msg_txt: str, src_node: str, dst_node: str) -> bytes:
     :param src_node: message originator
     :param dst_node: message recipient
     """
-    raise NotImplementedError
+    msg = bytearray()
+    msg.append(0x1)
+
+    # add source IP address
+    for num in src_node.split("."):
+        msg.append(int(num))
+    # add destination IP address
+    for num in dst_node.split("."):
+        msg.append(int(num))
+    # add encoded message
+    msg = msg + bytearray(msg_txt.encode())
+
+    return msg
 
 
 def parse_hello(msg: bytes, routing_table: dict) -> str:
@@ -78,7 +129,16 @@ def parse_hello(msg: bytes, routing_table: dict) -> str:
     :param routing_table: this router's routing table
     :returns the action taken as a string
     """
-    raise NotImplementedError
+    sender = str(msg[1]) + "." + str(msg[2]) + "." + str(msg[3]) + "." + str(msg[4])
+    destination = str(msg[5]) + "." + str(msg[6]) + "." + str(msg[7]) + "." + str(msg[8])
+    data = msg[9:].decode()
+
+    if destination == THIS_HOST:
+            print("Received {} from {}".format(data,sender))
+            return "Received {} from {}".format(data,sender)
+    else:
+        format_hello(data,sender,destination)
+        return "Forwarded {} to {}".format(data,routing_table[destination][1])
 
 
 def send_hello(msg_txt: str, src_node: str, dst_node: str, routing_table: dict) -> None:
@@ -90,7 +150,9 @@ def send_hello(msg_txt: str, src_node: str, dst_node: str, routing_table: dict) 
     :param dst_node: message recipient
     :param routing_table: this router's routing table
     """
-    raise NotImplementedError
+    message = format_hello(msg_txt,src_node,dst_node)
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+        sock.sendto(message, (routing_table[dst_node][1], BASE_PORT))
 
 
 def print_status(routing_table: dict) -> None:
@@ -127,8 +189,7 @@ def route(neighbors: set, routing_table: dict, timeout: int = 5):
 
 def main():
     """Main function"""
-    raise NotImplementedError
-
+    print("started")
 
 if __name__ == "__main__":
     main()
