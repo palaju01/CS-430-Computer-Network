6d86721fadc59a0ab883e73ada331fd81d4bf5f4
Gabriel Palacios <palaju01@luther.edu>
2020-11-15 01:14:32 -0600

    Final proyect completed

--- src/projects/routing/udp_router.py
+++ src/projects/routing/udp_router.py
@@ -47,6 +47,7 @@ def read_config_file(filename: str) -> Tuple[Set, Dict]:
     except IOError:
         raise FileNotFoundError("Could not find the specified configuration file {}".format(filename))
 
+
 def format_update(routing_table: dict) -> bytes:
     """
     Format update message
@@ -60,11 +61,27 @@ def format_update(routing_table: dict) -> bytes:
     for dest in routing_table:
         for num in dest.split("."):
             message.append(int(num))
-
         message.append(routing_table[dest][0])
-
-    #print("Update Message:", msg)
     return message
+
+
+def send_update(node: str, routing_table: dict) -> None:
+    """
+    Send update
+    
+    :param node: recipient of the update message
+    :param routing_table: this router's routing table
+    """
+    if node in routing_table:
+        # bind host IP with ICMP port
+        this_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        this_socket.bind((THIS_HOST,0))
+        destination_port = BASE_PORT + int(node.split(".")[-1])
+        
+        # format update and send it
+        msg = format_update(routing_table)
+        this_socket.sendto(msg, (node, destination_port))
+        print("Sent update to {}".format(node))
 
 
 def parse_update(msg: bytes, neigh_addr: str, routing_table: dict) -> bool:
@@ -83,27 +100,13 @@ def parse_update(msg: bytes, neigh_addr: str, routing_table: dict) -> bool:
         cost = msg[index+4]
         if addr in routing_table:
             if routing_table[addr][0] > routing_table[neigh_addr][0]+cost:
-                routing_table[addr][0] = routing_table[neigh_addr][0]+cost
+                routing_table[addr] = [routing_table[neigh_addr][0]+cost, neigh_addr]
                 updated = True
+        elif addr != THIS_HOST:
+            routing_table[addr] = [cost+routing_table[neigh_addr][0],neigh_addr]
+            updated = True
         index += 5
     return updated
-
-
-def send_update(node: str, routing_table: dict) -> None:
-    """
-    Send update
-    
-    :param node: recipient of the update message
-    :param routing_table: this router's routing table
-    """
-    # bind host IP with ICMP port
-    this_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    this_socket.bind((THIS_HOST,0))
-    destination_port = BASE_PORT + int(node.split(".")[-1])
-    
-    # format update and send it
-    msg = format_update(routing_table)
-    this_socket.sendto(msg, (node, destination_port))
 
 
 def format_hello(msg_txt: str, src_node: str, dst_node: str) -> bytes:
@@ -128,6 +131,26 @@ def format_hello(msg_txt: str, src_node: str, dst_node: str) -> bytes:
 
     return msg
 
+def send_hello(msg_txt: str, src_node: str, dst_node: str, routing_table: dict) -> None:
+    """
+    Send a message
+
+    :param mst_txt: message to send
+    :param src_node: message originator
+    :param dst_node: message recipient
+    :param routing_table: this router's routing table
+    """
+    if dst_node in routing_table:
+        # bind host IP with ICMP port
+        this_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        this_socket.bind((THIS_HOST,0))
+        destination_port = BASE_PORT + int(routing_table[dst_node][1].split(".")[-1])
+        
+        # format hello message and send it
+        msg = format_hello(msg_txt, src_node, dst_node)
+        this_socket.sendto(msg, (routing_table[dst_node][1], destination_port))
+        print("Sent hello message to {}".format(dst_node))
+
 
 def parse_hello(msg: bytes, routing_table: dict) -> str:
     """
@@ -145,27 +168,8 @@ def parse_hello(msg: bytes, routing_table: dict) -> str:
             print("Received {} from {}".format(data,sender))
             return "Received {} from {}".format(data,sender)
     else:
-        format_hello(data,sender,destination)
+        send_hello(data,sender,destination, routing_table)
         return "Forwarded {} to {}".format(data,routing_table[destination][1])
-
-
-def send_hello(msg_txt: str, src_node: str, dst_node: str, routing_table: dict) -> None:
-    """
-    Send a message
-
-    :param mst_txt: message to send
-    :param src_node: message originator
-    :param dst_node: message recipient
-    :param routing_table: this router's routing table
-    """
-    # bind host IP with ICMP port
-    this_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    this_socket.bind((THIS_HOST,0))
-    destination_port = BASE_PORT + int(routing_table[dst_node][1].split(".")[-1])
-    
-    # format hello message and send it
-    msg = format_hello(msg_txt, src_node, dst_node)
-    this_socket.sendto(msg, (routing_table[dst_node][1], destination_port))
 
 
 def print_status(routing_table: dict) -> None:
@@ -174,10 +178,111 @@ def print_status(routing_table: dict) -> None:
 
     :param routing_table: this router's routing table
     """
-    print("     {:^14} {:^10} {:^14}".format("Host","Cost","Via"))
+    print("   {:^14} {:^10} {:^14}".format("Host","Cost","Via"))
     for router in routing_table:
-        print("     {:^14} {:^10} {:^14}".format(router,routing_table[router][0],routing_table[router][1]))
+        print("   {:^14} {:^10} {:^14}".format(router,routing_table[router][0],routing_table[router][1]))
 
+
+def format_status_request(dst_node: str) -> bytes:
+    """
+    Format update message
+
+    :param dst_node: message recipient
+    :returns the formatted message
+    """
+    address = list(dst_node.split("."))
+    message = struct.pack("!BBBBB", 2, int(address[0]), int(address[1]), int(address[2]), int(address[3]))
+    return message
+
+
+def send_status_request(dst_node: str, routing_table: dict) -> None:
+    """
+    Send status request
+
+    :param routing_table: routing table of this router
+    """
+    if dst_node in routing_table:
+        # bind host IP with a port
+        this_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        this_socket.bind((THIS_HOST,0))
+        destination_port = BASE_PORT + int(routing_table[dst_node][1].split(".")[-1])
+        
+        # format status request message and send it
+        msg = format_status_request(THIS_HOST)
+        this_socket.sendto(msg, (routing_table[dst_node][1], destination_port))
+        print("Sent status request to {}".format(dst_node))
+
+
+def parse_status_request(msg: bytes, routing_table: dict) -> str:
+    """
+    Parse the HELLO message
+
+    :param msg: message
+    :param routing_table: this router's routing table
+    :returns the action taken as a string
+    """
+    source = str(msg[1]) + "." + str(msg[2]) + "." + str(msg[3]) + "." + str(msg[4])
+    print("Received status request from {}".format(source))
+    send_status_response(source,routing_table)
+    return "Received status request from {}".format(source)
+
+
+def format_status_response(dst_node: str, routing_table: dict) -> bytes:
+    """
+    Format status response message
+
+    :param routing_table: routing table of this router
+    :returns the formatted message
+    """
+    message = bytearray()
+    message.append(0x3)
+
+    # add destination address
+    for dst in list(dst_node.split(".")):
+        message.append(int(dst))
+    
+    # add routing table data
+    for dest in routing_table:
+        message.append(routing_table[dest][0])
+        for num in dest.split("."):
+            message.append(int(num))
+    return message
+
+
+def send_status_response(dst_node: str, routing_table: dict) -> None:
+    """
+    Send status response message
+
+    :param routing_table: routing table of this router
+    """
+    if dst_node in routing_table:
+        #print(dst_node, routing_table)
+        # bind host IP with a port
+        this_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        this_socket.bind((THIS_HOST,0))
+        destination_port = BASE_PORT + int(routing_table[dst_node][1].split(".")[-1])
+        
+        # format status request message and send it
+        msg = format_status_response(dst_node,routing_table)
+        this_socket.sendto(msg, (routing_table[dst_node][1], destination_port))
+        print("Sent status response to {}".format(dst_node))
+
+
+def parse_status_response(msg: bytes, routing_table: dict) -> str:
+    """
+    Parse the HELLO message
+
+    :param msg: message
+    :param routing_table: this router's routing table
+    :returns the action taken as a string
+    """
+    destination = str(msg[1]) + "." + str(msg[2]) + "." + str(msg[3]) + "." + str(msg[4])
+    if destination == THIS_HOST:
+            print("Received status response")
+            return "Received status response"
+    else:
+        send_status_response(destination,routing_table)
+        return "Forwarded status response to {}".format(routing_table[destination][1])
 
 
 def route(neighbors: set, routing_table: dict, timeout: int = 5):
@@ -206,10 +311,6 @@ def route(neighbors: set, routing_table: dict, timeout: int = 5):
     # print router status 
     print_status(routing_table)
 
-    # send updates to all neighbours at start
-    for neighbor in neighbors:
-        send_update(neighbor, routing_table)
-
     while True:
         # The following section determines when to "randomly" send messages to other routers.
 
@@ -223,12 +324,13 @@ def route(neighbors: set, routing_table: dict, timeout: int = 5):
         # random update message
         elif rand == 10:
             #print("random update message")
-            for neighbor in neighbors:
-                send_update(neighbor,routing_table)
+            dst = random.choice(list(routing_table.keys()))
+            send_update(dst,routing_table)
         # random status request message - optional
         elif rand == 15:
             #print("random status request")
-            pass
+            dst = random.choice(list(routing_table.keys()))
+            send_status_request(dst,routing_table)
 
 
         # The following section process incoming messages from other routers.
@@ -237,10 +339,8 @@ def route(neighbors: set, routing_table: dict, timeout: int = 5):
 
         for sckt in new_messages[0]:
             msg, addr = sckt.recvfrom(1024)
-            print(msg[0])
             if msg[0] == 0:
                 # Update message
-                print("Received update message")
                 updated = parse_update(msg, addr[0], routing_table)
                 if updated:
                     print_status(routing_table)
@@ -248,14 +348,13 @@ def route(neighbors: set, routing_table: dict, timeout: int = 5):
                         send_update(neighbor,routing_table)
             elif msg[0] == 1:
                 # Hello message
-                print("Received hello message")
                 parse_hello(msg, routing_table)
             elif msg[0] == 2:
-                # Status request - optional
-                print("Status request")
+                # Status request
+                parse_status_request(msg, routing_table)
             elif msg[0] == 3:
-                # Status response - optional
-                print("Status response")
+                # Status response
+                parse_status_response(msg, routing_table)
 
             # If the type is not 0 to 3, the packet will be dropped.
 
